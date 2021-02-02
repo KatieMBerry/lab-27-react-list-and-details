085cffe74261c21065a4f3755a0f38dd52ab8231
Katie <katieberrym@gmail.com>
2021-02-02 13:59:51 -0800

    added react-router-dom

--- src/components/characters/CharacterDetail.jsx
+++ src/components/characters/CharacterDetail.jsx
@@ -1,1 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
+//function component for presentational items
+const CharacterDetail = ({ name, species, image }) => (
+    <figure>
+
+    </figure>
+
+);
+
+CharacterDetail.propTypes = {
+    name: PropTypes.string.isRequired,
+    species: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired
+};
+
+export default CharacterDetail;
